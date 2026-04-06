e8f875229e7f77c6769df5b4bd1ec66acca9c2f7
arnold therigan <arnold.therigan@bareksa.com>
2026-04-06 16:14:52 +0700

    chore: include env.js in repo

---
+++ env.js
@@ -0,0 +1,8 @@
+/**
+ * ==============================================
+ * ENV CONFIG — Jangan commit file ini ke GitHub!
+ * ==============================================
+ * File ini berisi URL Google Apps Script.
+ * Sudah di-gitignore agar tidak ter-upload.
+ */
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxyh5OcWm7Jr0e196R9bg_FUPqT58uULQfWxniIUi3wfJ9KL83GkzKDeb7OB0uzI1eaDQ/exec';
