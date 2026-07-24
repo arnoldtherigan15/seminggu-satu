3039dd64f3c77f7bb829f2f7be1ecf4b7f7225f5
arnold therigan <arnold.therigan@bareksa.com>
2026-07-24 17:56:13 +0700

    style(sahabat): takeout sticker siput-jeruk (str-9) dari modal surat Mochi

--- sahabat/main.js
+++ sahabat/main.js
@@ -2111,7 +2111,6 @@ async function openMochiPrompt() {
     modal.innerHTML =
         '<div class="mochi-box">' +
         '<button class="mp-close" id="mpClose" aria-label="Tutup">✕</button>' +
-        '<img class="mp-imgstk" src="../images/sticker/str-9.png" style="width:62px;top:6px;left:8px;transform:rotate(-14deg);" alt="">' +
         '<img class="mp-imgstk" src="../images/sticker/str-6.png" style="width:62px;bottom:8px;left:6px;transform:rotate(-8deg);" alt="">' +
         '<img class="mp-imgstk" src="../images/sticker/str-11.png" style="width:58px;bottom:10px;right:8px;transform:rotate(11deg);" alt="">' +
         '<div class="mp-head">💌 Surat dari Mochi</div>' +
